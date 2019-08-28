e5187881402d659a49f1647396388c3afb858139
golotenko <54649203+golotenko@users.noreply.github.com>
2019-08-28 23:52:47 +0000

    Create index.js

---
+++ index.js
@@ -0,0 +1,3 @@
+module.exports = {
+    about: 'this project does not provide a single API, but rather just holds various useful module files that are supposed to be used through require("ota-utils/Path/To/Module.js")',
+};
