a8dcfac9593b4c9c3a1b1e62f0c2503b52f64693
golotenko <54649203+golotenko@users.noreply.github.com>
2019-12-06 05:12:43 +0200

    Fix: missing `^` in a regex made it not obvious that the issue was in the parser in last commit

--- src/text_format_processing/apollo/ticketing_masks/NmeScreenParser.js
+++ src/text_format_processing/apollo/ticketing_masks/NmeScreenParser.js
@@ -175,7 +175,7 @@ class NmeScreenParser {
 			return parsed;
 		} else if (php.preg_match(/^\s*;?([.OX])\s+([A-Z]{3})(?:\s|\.)+VOID(?:\s|\.)+$/, line, matches = [])) {
 			return {type: 'void', departureCity: matches[2], isStopover: matches[1] !== 'X'};
-		} else if (php.preg_match(/(?:\s|\.)+VOID(?:\s|\.)+$/, line)) {
+		} else if (php.preg_match(/^(?:\s|\.)+VOID(?:\s|\.)+$/, line)) {
 			return {type: 'void', departureCity: null, isStopover: true};
 		} else {
 			return null;
