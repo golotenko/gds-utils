57f980974784f73a65899eb862f952eb35713304
Alexander Golotenko <alexandergolotenko@gmail.com>
2020-03-13 00:19:29 +0200

    Update var name in a test

--- tests/text_format_processing/sabre/pnr/ItineraryParserTest.js
+++ tests/text_format_processing/sabre/pnr/ItineraryParserTest.js
@@ -6,15 +6,14 @@ let php = require('enko-fundamentals/src/Transpiled/php.js');
 class ItineraryParserTest extends require('enko-fundamentals/src/Transpiled/Lib/TestCase.js')
 {
 	provideTestDumpList()  {
-		let $list;
-		$list = [];
-		$list.push([
+		const list = [];
+		list.push([
 			php.implode(php.PHP_EOL, [
 				' 1 DL 856V 22SEP Q SANJFK*HK6  1139A 815P \/DCDL*GYJAFF \/E      ',
 				' 2 DL 474V 22SEP Q JFKVCE*HK6   917P1135A  23SEP F             ',
 				'                                               \/DCDL*GYJAFF \/E  ',
 				' 3 DL 475X 07OCT F VCEJFK*HK6   120P 450P \/DCDL*GYJAFF \/E      ',
-				' 4 DL1576X 07OCT F JFKSAN*HK6   730P1044P \/DCDL*GYJAFF \/E     '
+				' 4 DL1576X 07OCT F JFKSAN*HK6   730P1044P \/DCDL*GYJAFF \/E     ',
 			]),
 			[
 				{
@@ -46,12 +45,12 @@ class ItineraryParserTest extends require('enko-fundamentals/src/Transpiled/Lib/
 					'destinationTime': {'parsed': '22:44'},
 					'eticket': true,
 				},
-			]
+			],
 		]);
-		$list.push([
+		list.push([
 			php.implode(php.PHP_EOL, [
 				'1 EL 409L 8SEP 5 SVOCFU SS1  0420  0735          E',
-				'2 EL 408L 14SEP 5 CFUDME SS1  0855  1210          E'
+				'2 EL 408L 14SEP 5 CFUDME SS1  0855  1210          E',
 			]),
 			[
 				{
@@ -90,16 +89,16 @@ class ItineraryParserTest extends require('enko-fundamentals/src/Transpiled/Lib/
 					'operatedBy': '',
 					'segmentType': 'AIR',
 				},
-			]
+			],
 		]);
-		$list.push([
+		list.push([
 			php.implode(php.PHP_EOL, [
 				'1 UA 1769K 22DEC Q MCOEWR SS1  120P 400P   22DEC F',
 				'2 ET 509O 22DEC Q EWRLFW SS1   815P 1130A  23DEC F E',
 				'3 KP  24T 23DEC F LFWLOS SS1   130P  330P          E',
 				'4 KP  51T 06JAN F LOSLFW SS1  1100A 1100A          E',
 				'5 ET 508O 06JAN F LFWEWR SS1   100P  615P          E',
-				'6 UA 1149W 6JAN F  EWRMCO SS1  900P  1148P'
+				'6 UA 1149W 6JAN F  EWRMCO SS1  900P  1148P',
 			]),
 			[
 				{
@@ -168,9 +167,9 @@ class ItineraryParserTest extends require('enko-fundamentals/src/Transpiled/Lib/
 					'destinationAirport': 'MCO',
 					'segmentType': 'AIR',
 				},
-			]
+			],
 		]);
-		$list.push([
+		list.push([
 			php.implode(php.PHP_EOL, [
 				' 1 UA 801K 01DEC Q STLORD HK1  1128A 1249P \/DCUA \/E            ',
 				'ADV PAX FLT ARRIVES TERMINAL-1                                  ',
@@ -189,9 +188,9 @@ class ItineraryParserTest extends require('enko-fundamentals/src/Transpiled/Lib/
 						'OPERATED BY UNITED AIRLINES                                    ',
 					]),
 				},
-			}
+			},
 		]);
-		$list.push([
+		list.push([
 			php.implode(php.PHP_EOL, [
 				' 1 AF1169L 09MAY T MANCDG*  3\/1 HK2   745P 1010P \/DCAF*5JV97Q',
 				'                                                            \/E',
@@ -203,7 +202,7 @@ class ItineraryParserTest extends require('enko-fundamentals/src/Transpiled/Lib/
 			]),
 			{'0': {'marriage': '3'},'2': {'marriage': '4'}},
 		]);
-		$list.push([
+		list.push([
 			php.implode(php.PHP_EOL, [
 				' 1 AA 125I 11APR T DFWHKG HK2  1040A  410P  12APR W SPM HRS',
 				'                                               \/DCAA*ENQFIM \/E',
@@ -216,7 +215,7 @@ class ItineraryParserTest extends require('enko-fundamentals/src/Transpiled/Lib/
 				{'confirmationAirline': 'AA','confirmationNumber': 'ENQFIM'},
 			],
 		]);
-		$list.push([
+		list.push([
 			php.implode(php.PHP_EOL, [
 				'NO NAMES',
 				' 1 PS 898S 10DEC S KIVKBP SS1   710A  820A \/DCPS \/E',
@@ -245,9 +244,9 @@ class ItineraryParserTest extends require('enko-fundamentals/src/Transpiled/Lib/
 					'agencyIataCode': '05578602',
 				},
 				{'segmentNumber': 4,'destinationAirport': 'HEL'},
-			]
+			],
 		]);
-		$list.push([
+		list.push([
 			php.implode(php.PHP_EOL, [
 				'1 D81843H 14DEC F SWFDUB SS1 1200N 1125P \/DCD8 \/E',
 				'2 EI 680W 15DEC J DUBGVA SS1 630A 940A \/DCEI \/E',
@@ -261,13 +260,13 @@ class ItineraryParserTest extends require('enko-fundamentals/src/Transpiled/Lib/
 				{'segmentNumber': 3},
 				{'segmentNumber': 4, 'destinationTime': {'raw': '1100P', 'parsed': '23:00'}},
 				{'segmentNumber': 5},
-			]
+			],
 		]);
-		$list.push([
+		list.push([
 			'OPERATED BY UNITED AIRLINES',
 			[],
 		]);
-		return $list;
+		return list;
 	}
 
 
